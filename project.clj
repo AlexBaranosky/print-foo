2fd7ebdd2ddd865d55542839e4fa84425547d0a1
Alex Baranosky <alexander.baranosky@gmail.com>
2013-02-27 10:09:06 -0800

    upgrade away from buggy dev deps

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject print-foo "0.3.0"
+(defproject print-foo "0.3.1"
   :description "A set of useful print-debugging tools"
   :url "https://github.com/AlexBaranosky/print-foo"
   :license {:name "MIT License"
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.5.0-RC16"]
                  [gui-diff "0.4.0"]]
   :profiles {:dev {:dependencies [[jonase/kibit "0.0.3"]
-                                  [slamhound "1.3.2"]]
+                                  [slamhound "1.3.3"]]
                    :plugins [[jonase/eastwood "0.0.2"]]}
              :1.3.0 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.4.0 {:dependencies [[org.clojure/clojure "1.4.0"]]}
