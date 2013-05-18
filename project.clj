ac7a5a095fb6d451ce500b52928cacaf07683462
Alex Baranosky <alexander.baranosky@gmail.com>
2013-05-18 15:57:29 -0700

    up to non-broken gui-diff version

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject print-foo "0.3.4"
+(defproject print-foo "0.3.5"
   :description "A set of useful print-debugging tools"
   :url "https://github.com/AlexBaranosky/print-foo"
   :license {:name "MIT License"
             :url "http://mit-license.org/"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [gui-diff "0.6.0"]
+                 [gui-diff "0.6.1"]
 		         [org.clojure/math.combinatorics "0.0.4"]
                  [org.clojars.runa/conjure "2.2.0"]]
   :profiles {:dev {:dependencies [[jonase/kibit "0.0.3"]
