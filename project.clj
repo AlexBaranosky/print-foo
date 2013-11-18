2fbf3089326f4d678a42ea481acb1bc29ed6fa01
Alex Baranosky <alexander.baranosky@gmail.com>
2013-11-18 13:51:05 -0800

    bumpe gui-diff dependency to one that fixed the run-tests++ bug

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject print-foo "0.4.6"
+(defproject print-foo "0.4.7"
   :description "A set of useful print-debugging tools"
   :url "https://github.com/AlexBaranosky/print-foo"
   :license {:name "MIT License"
             :url "http://mit-license.org/"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [gui-diff "0.6.2"]]
+                 [gui-diff "0.6.4"]]
   :profiles {:dev {:dependencies [[jonase/kibit "0.0.3"]
                                   [org.clojars.runa/conjure "2.2.0"]
                                   [slamhound "1.3.3"]]
