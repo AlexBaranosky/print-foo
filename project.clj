c6962f46ee7b15a18a4a56b7f8a5bf022bf08f5f
Alex Baranosky <alexander.baranosky@gmail.com>
2014-05-03 11:08:36 -0700

    bump jar version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject print-foo "0.5.0"
+(defproject print-foo "0.5.1"
   :description "A set of useful print-debugging tools"
   :url "https://github.com/AlexBaranosky/print-foo"
   :license {:name "MIT License"
